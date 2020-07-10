33f0bf02176ae314e0ab0603cc68a5fb3ca5f1da
karim <bagayogoyogo@gmail.com>
2020-07-10 15:57:49 +0200

    lets go

---
+++ Numpy_floor_ceil_round.py
@@ -0,0 +1,17 @@
+from numpy import *
+a = np.sin(np.deg2rad(30))
+b = np.cos(np.deg2rad(30))
+c = np.tan(np.deg2rad(30))
+print(a,b,c)
+
+# Ceil and floor and their utilisties
+ a =ceil(3.68825)  #get max int value
+ b = floor(3.68825) #get min int value
+ print (a)
+ print (b)
+
+#  Mod and power functions
+a = mod(20,3)
+b = power(2,2)
+print(a)
+print(b)
